3180b701e721d0c1eba24f37aa2d7602be610f54
ruguoxiansheng <by_wwj@163.com>
2017-12-27 13:31:29 +0800

    aa

---
+++ src/main/java/com/my/SSOServer.java
@@ -0,0 +1,13 @@
+package com.my;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SSOServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SSOServer.class, args);
+	}
+
+}
